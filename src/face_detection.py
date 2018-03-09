7aac82ce616968da83f36216863093b75bfba65f
adadesions <adadesions@gmail.com>
2018-03-10 03:04:51 +0700

    Done face detection and crop boundary

---
+++ src/face_detection.py
@@ -0,0 +1,53 @@
+import os
+import dlib
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib.patches as patches
+from skimage import io, transform
+
+
+# Set path
+PRO_DIR = os.path.dirname(os.path.abspath('../'+__file__))
+DATA_DIR = os.path.join(PRO_DIR, 'data')
+
+
+def cropped_images(image_path=None, extend_left=50, width_param=2, heigh_param=5):
+    # Reading an image
+    if image_path == None:
+        image_path = os.path.join(DATA_DIR, '001.jpg')
+        
+    image = io.imread(image_path)
+
+    # Face Detector
+    detector = dlib.get_frontal_face_detector()
+    dets = detector(image, 1)
+    print('Number of faces detected = {}'.format(len(dets)))
+
+    # Initial Cropped list
+    cropped = []
+
+    for i, d in enumerate(dets):
+        left, top = d.left()-extend_left, d.top()
+        right, bottom = d.right(), d.bottom()
+        d_point = (left, top)
+        width = (right - left)*width_param
+        heigh = (bottom - top)*heigh_param
+
+        cropped += [image[top:top+heigh, left:left+width]]
+
+    return cropped
+
+
+def main():
+    # Matplotlib Configurations
+    fig = plt.figure()
+    ax = fig.add_subplot(111, aspect='equal')
+
+    cropped = cropped_images()
+    for crop in cropped:
+        plt.imshow(crop, cmap='gray')
+        plt.show()
+        
+
+if __name__ == '__main__':
+    main()
