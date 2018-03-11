518672114a900a4c5b1b74f7c3af2c4646097235
adadesions <adadesions@gmail.com>
2018-03-12 02:22:08 +0700

    Add reading images collection

--- src/open_marathon.py
+++ src/open_marathon.py
@@ -1,19 +1,38 @@
+import sys
 import matplotlib.pyplot as plt
 from face_detection import cropped_images
 from ocr import ada_ocr
 
+def image_collection(file_path=None):
+    if file_path == None:
+        file_path = '../data/test_file.txt'
+    file = open(file_path, 'r')
+    file_list = file.readlines()
+    print(file_list)
 
 def main():
+    # Initialization
+    processed_data = {}
     # Matplotlib Configurations
-    fig = plt.figure()
-    ax = fig.add_subplot(111, aspect='equal')
+    # fig = plt.figure()
+    # ax = fig.add_subplot(111, aspect='equal')
 
-    cropped = cropped_images(image_path='../data/015.jpg', see_full=False)
+    # Image file configuration
+    image_path = sys.argv[1]
+    filename = image_path.split('/')[-1]
+
+    cropped = cropped_images(image_path=image_path, see_full=False)
     predicted = ada_ocr(images=cropped)
-    print(predicted)
+
+    # Save file
+    processed_data[filename] = predicted
+    print(processed_data)
+
+    # print(predicted)
     # for crop in cropped:
     #     plt.imshow(crop, cmap='gray')
     #     plt.show()
+    image_collection()
 
 
 if __name__ == '__main__':
